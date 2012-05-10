6e5df9c8a6a8362bda4ddb421fa306631eb850f1
H. Watanabe <kaityo@users.sourceforge.jp>
2012-05-10 14:56:56 +0900

    Initial commit

---
+++ draw3d.h
@@ -0,0 +1,40 @@
+#ifndef draw3d_h
+#define draw3d_h
+#include "bm.h"
+//----------------------------------------------------------------------
+struct Point{
+  int X, Y; 
+  Point();
+  Point(int _x, int _y){
+    X = _x;Y = _y;
+  };
+};
+//----------------------------------------------------------------------
+class Draw3D{
+  private:
+    WindowsBitmap *canvas;
+    int particleNumber;
+    double L;
+    double Theta, Phi;
+    double Sigma;
+    static double *depth;
+    int *sorted_index;
+    int SX, SY;
+    int Width, Height;
+    double Magnification;
+    Point GetPoint(double x, double y, double z);
+    void DrawLine(double x1, double y1, double z1,double x2, double y2, double z2);
+    void DrawAxis(void);
+    void SortParticles(void);
+  public:
+    Draw3D(double ss, int is);
+    ~Draw3D(void);
+    void SetPhi(double p){Phi = p;};
+    void SetTheta(double t){Theta = t;};
+    void SetSigma(double s){Sigma = s;};
+    void Draw(int n, double *qx, double *qy, double *qz);
+    void SaveToFile(const char *filename){canvas->SaveToFile(filename);};
+    static bool Compare(int i, int j);
+};
+//----------------------------------------------------------------------
+#endif
---
+++ main.cc
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <stdlib.h>
+#include "draw3d.h"
+//----------------------------------------------------------------------
+const int N = 10000;
+double qx[N],qy[N],qz[N];
+//----------------------------------------------------------------------
+double
+myrand(void){
+  return (double)rand()/(double)RAND_MAX;
+}
+//----------------------------------------------------------------------
+void
+add(int &n,double x, double y, double z){
+  qx[n] = x;
+  qy[n] = y;
+  qz[n] = z;
+  n++;
+}
+//----------------------------------------------------------------------
+int
+main(void){
+  const double L = 20.0;
+  int n = 0;
+  double cx = L*0.5;
+  double cy = L*0.5;
+  double cz = L*0.5;
+  double r = 6.0; 
+  double step = 1.1;
+  double sh = step*0.5;
+  int is = static_cast<int>(r/step)+1;
+  for(int ix=-is;ix<is;ix++){
+    for(int iy=-is;iy<is;iy++){
+      for(int iz=-is;iz<is;iz++){
+        double x,y,z;
+        x = ix*step;
+        y = iy*step;
+        z = iz*step;
+        if(x*x+y*y+z*z < r*r) add(n,x+cx,y+cy,z+cz);
+        x = ix*step+sh;
+        y = iy*step+sh;
+        z = iz*step;
+        if(x*x+y*y+z*z < r*r) add(n,x+cx,y+cy,z+cz);
+        x = ix*step+sh;
+        y = iy*step;
+        z = iz*step+sh;
+        if(x*x+y*y+z*z < r*r) add(n,x+cx,y+cy,z+cz);
+        x = ix*step;
+        y = iy*step+sh;
+        z = iz*step+sh;
+        if(x*x+y*y+z*z < r*r) add(n,x+cx,y+cy,z+cz);
+      }
+    }
+  } 
+  Draw3D *draw = new Draw3D(L,400);
+  draw->Draw(n,qx,qy,qz);
+  draw->SaveToFile("test.bmp");
+  delete draw;
+}
+//----------------------------------------------------------------------
